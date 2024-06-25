90046e274f0c4c1d09a491932734c577837fa0e6
Victoria Usenko <usenko.vika@gmail.com>
2024-06-25 22:20:49 +0300

    change in structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
 import { useState, useEffect } from "react";
 
-import "modern-normalize";
-import "../../index.css";
+
+// import "./index.css";
 import "./App.css";
 
-import Description from "../Description/Description";
-import Options from "../Options/Options";
-import Feedback from "../Feedback/Feedback";
-import Notification from "../Notification/Notification";
+import Description from "./components/Description/Description";
+import Options from "./components/Options/Options";
+import Feedback from "./components/Feedback/Feedback";
+import Notification from "./components/Notification/Notification";
 
 export default function App() {
   const [feedbacks, setFeedbacks] = useState(() => {
